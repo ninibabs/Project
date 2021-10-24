9b2c01fe17b95fbd1596a14370633906e94f12f5
ninibabs <ninibabalola03@gmail.com>
2021-10-24 18:11:03 +0100

    Simple

---
+++ Assigment.js
@@ -0,0 +1,102 @@
+
+
+//const obj = {info:"nini", age:9, school:"Grange(No longer in Greensprings)"}
+
+//const  {info,age,school}  =obj
+
+//console.log("My name is " + info + " and i am " + age + " and my school is " + school )
+
+
+let arr = [1,20,3,4,50,6,7,8,9,0]
+
+const [nini,,,,,,gogeta] =arr
+
+ console.log(nini)
+ console.log(gogeta)  
+ 
+ 
+ arr.map((item) =>{
+console.log("this is: ", item)
+ })
+  
+
+ let arr1 = [111,2220,333,444,50555,6555,557,855,988,11220]
+
+const [iniolu,,,,,,gogetaka] =arr
+
+ console.log(iniolu)
+ console.log(gogetaka)  
+ 
+ 
+ arr.map((item) =>{
+console.log("this is: ", item)
+ })
+  
+
+
+
+ let arr2 = [144,24440,34444,44,5440,644,756,877,989,022]
+
+ const [pay,,,,,,ggeta] =arr
+ 
+  console.log(pay)
+  console.log(ggeta)  
+  
+  
+  arr.map((item) =>{
+ console.log("this is: ", item)
+  })
+   
+
+
+
+  let arr3 = [14,267340,3874,4749,5084,685,784,837984,934872,0347057373507342170345]
+
+  const [nni,,,,,,geta] =arr
+  
+   console.log(nni)
+   console.log(geta)  
+   
+   
+   arr.map((item) =>{
+  console.log("this is: ", item)
+   })
+
+
+
+    
+   let arr4 = [153,20346359,374,473,5530,68549,937,458,839,050]
+
+   const [ini,,,,,,eta] =arr
+   
+    console.log(ini)
+    console.log(eta)  
+    
+    
+
+    arr.map((item) =>{
+   console.log("this is: ", item)
+    })
+     
+
+
+
+
+    let arr5 = [1854,290,3483,4,58740,6,786,85,90,650]
+
+    const [ecoBank,,,,,,secre] =arr5
+    
+     console.log(ecoBank)
+     console.log(secre)  
+     
+     
+     arr.map((item) =>{
+    console.log("this is: ", item)
+     })
+      
+         
+
+   
+
+
+
